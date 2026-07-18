a7465f217f9f6d6cf4193109863a9120a5e9d709
andrea-urciuolo <a.urciuolo5@studenti.unisa.it>
2026-07-18 15:05:26 +0200

    fix: urgent fix for mobile users

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+window.addEventListener('error', (e) => {
+  const errorMessage = e.message || '';
+  if (
+    errorMessage.includes('Failed to fetch dynamically imported module') ||
+    errorMessage.includes('ChunkLoadError')
+  ) {
+    console.warn('Rilevata vecchia cache o chunk mancante. Ricarico l\'app...');
+    window.location.reload();
+  }
+}, true);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
